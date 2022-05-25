00a4d681363a2e018300d4a232ad819af8238875
aaron2147m <aaronasuarez@yahoo.com>
2022-05-25 19:51:15 -0400

    js2.js

---
+++ js/js2.js
@@ -0,0 +1,13 @@
+//Your code will determine if the number passed is even (or not).
+
+//The function needs to return either a true or false.
+
+function testEven(n) {
+    if(n % 2 === 0){
+      return true;
+    }else{
+      return false;
+    
+    }
+    }
+  

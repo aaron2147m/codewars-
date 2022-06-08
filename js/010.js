9763f73585d99faffd1e6f5367dd44b34269ac39
aaron2147m <aaronasuarez@yahoo.com>
2022-06-07 22:09:30 -0400

    011.js

--- js/010.js
+++ js/010.js
@@ -1,5 +1,5 @@
 //What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
 
-unction addLength(str) {
+function addLength(str) {
     return str.split(' ').map(v => `${v} ${v.length}` );
   }

08c628446a73ed721658e5fe0579b115832b4c42
aaron2147m <aaronasuarez@yahoo.com>
2022-06-08 22:37:51 -0400

    013.js

---
+++ js/013.js
@@ -0,0 +1,9 @@
+//We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not
+
+function plural(n) {
+    // ...
+    if (n != 1){
+      return true;
+    }
+      return false;
+  }

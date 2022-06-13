fec9694fd197f678ca86312b5bf7508b3027708e
aaron2147m <aaronasuarez@yahoo.com>
2022-06-12 22:06:37 -0400

    017.js

---
+++ js/017.js
@@ -0,0 +1,6 @@
+//Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+function usdcny(usd) {
+    let con = usd * 6.75
+    return `${con.toFixed(2)} Chinese Yuan`
+  }

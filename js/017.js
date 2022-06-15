dea017fb615b98530406f4bc9a94aa04bf71e8e7
aaron2147m <aaronasuarez@yahoo.com>
2022-06-14 22:09:23 -0400

    018.js

--- js/017.js
+++ js/017.js
@@ -1,9 +1,4 @@
-//Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
-
-function usdcny(usd) {
-    let con = usd * 6.75
-    return `${con.toFixed(2)} Chinese Yuan`
-  function chromosomeCheck(sperm) {
+function chromosomeCheck(sperm) {
     if (sperm === "XX") {
       return "Congratulations! You're going to have a daughter."
     } else if (sperm === "XY") {

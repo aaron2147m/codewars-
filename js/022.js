166ef56c16ac2f6dc9b0a1a886d1c39f85de9539
aaron2147m <aaronasuarez@yahoo.com>
2022-06-18 22:21:18 -0400

    022.js

---
+++ js/022.js
@@ -0,0 +1,23 @@
+//You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+
+
+
+function updateLight(current) {
+
+    switch (current) {
+      case 'green':
+        return 'yellow';
+        break;
+      case 'yellow':
+        return 'red';
+        break;
+      case 'red':
+        return 'green';
+        break;
+      default:
+        throw 'Error: wrong input';
+        break;
+    }
+    
+  }

5259fa5416a31c302eafc63d1d42bad41da3e76f
aaron2147m <aaronasuarez@yahoo.com>
2022-06-26 11:08:39 -0400

    028.js

---
+++ js/027.js
@@ -0,0 +1,12 @@
+function checkTheBucket(bucket){
+    if (bucket.includes('gold')) {
+     return true;
+   } 
+   else if (bucket.includes()) {
+     return false; 
+   }
+   else{
+     return false; 
+   }
+   
+}

91b22a597e2eddfa9e51f5446e1c0537f91ae93e
aaron2147m <aaronasuarez@yahoo.com>
2022-07-31 22:49:57 -0400

    051.js

---
+++ js/051.js
@@ -0,0 +1,6 @@
+//You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10
+
+
+function orderOperations () {
+    return (2 + 2) * (2 + 2) * 2
+  }

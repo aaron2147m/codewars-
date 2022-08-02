86b90b92c508ba67ff3568c9fa79426771c3a91f
aaron2147m <aaronasuarez@yahoo.com>
2022-08-01 22:00:33 -0400

    052.js

---
+++ js/052.js
@@ -0,0 +1,25 @@
+//Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+
+
+
+function logicalCalc(array, op)
+{ 
+  var result = array[0];
+  for(var i = 1; i < array.length; i++)
+  {
+    if(op == "AND")
+    {
+      result = result && array[i];
+    }
+    if(op == "OR")
+    {
+      result = result || array[i];
+    }
+    if(op == "XOR")
+    {
+      result = result != array[i];
+    }
+  }
+  return result;
+}

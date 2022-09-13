1358c4ea0e77150c3ca58748f2e99834051aefd3
aaron2147m <aaronasuarez@yahoo.com>
2022-09-12 23:10:48 -0400

    071.js

---
+++ js/071.js
@@ -0,0 +1,14 @@
+function typeValidation(variable, type) {
+    if (typeof variable === 'number' && type === 'number') {
+      return true;
+    } else if (typeof variable === 'undefined' && type === 'undefined') {
+      return true;
+    } else if (typeof variable === 'string' && type === 'string') {
+      return true;
+    } else if (typeof variable === 'boolean' && type === 'boolean') {
+      return true;
+    } else if (typeof variable === 'object' && type === 'object') {
+      return true;
+    }
+    return false;
+  }

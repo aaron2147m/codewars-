77399e138bc46a5905dd3f85019d2b6d37ad9ede
aaron2147m <aaronasuarez@yahoo.com>
2022-09-23 23:08:06 -0400

    078.js

---
+++ js/078.js
@@ -0,0 +1,11 @@
+
+//write an algorithm that gets you from the input to the output.
+
+
+function preFizz(n) {
+     let arr = [];
+     for(let i = 1; i < n + 1; i++){
+      arr.push(i);
+    }
+    return arr;
+  }

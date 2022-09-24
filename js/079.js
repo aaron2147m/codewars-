ec063ea26baa8f41e1965da1b90ef88c4e743ad1
aaron2147m <aaronasuarez@yahoo.com>
2022-09-24 18:38:24 -0400

    079.js

---
+++ js/079.js
@@ -0,0 +1,9 @@
+//create a phone number 
+
+function createPhoneNumber(numbers){
+    numbers = numbers.join('');
+    return '(' + numbers.substring(0, 3) + ') ' 
+        + numbers.substring(3, 6) 
+        + '-' 
+        + numbers.substring(6);
+  }

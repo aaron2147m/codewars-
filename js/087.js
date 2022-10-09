9bb2e746e9a2218c9f609d985b38d29e1dfeb93a
aaron2147m <aaronasuarez@yahoo.com>
2022-10-08 20:39:39 -0400

    088.js

--- js/087.js
+++ js/087.js
@@ -1,4 +1,4 @@
 function blackAndWhite(arr){
-    //coding here...
+  
     return `It's a ${Array.isArray(arr) ? arr.indexOf(5) != -1 && arr.indexOf(13) != -1 ? 'black' : 'white' : 'fake'} array`;
   }

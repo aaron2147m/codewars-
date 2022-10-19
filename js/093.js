d0c5f1d5946aa63f2aba650ca2d2ef20d3bd48f9
aaron2147m <aaronasuarez@yahoo.com>
2022-10-18 22:55:59 -0400

    094.js

---
+++ js/093.js
@@ -0,0 +1,4 @@
+//he function takes in 2 inputs x and y, and should return x to the power of y
+function power(x,y){
+    return x**y;
+  }
---
+++ js/094.js
@@ -0,0 +1,13 @@
+// added postive integers in a array 
+
+function positiveSum(arr) {
+    let total = 0
+    
+    for(let i = 0; i < arr.length; i++) {
+      
+      if( arr[i] > 0){
+        total +=arr[i]
+      }
+       }
+  return total;
+  }

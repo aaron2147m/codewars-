18c378b34339f3cd53e9847d98322d221def0929
aaron2147m <aaronasuarez@yahoo.com>
2022-10-20 22:21:33 -0400

    096.js

---
+++ js/096.js
@@ -0,0 +1,21 @@
+function howMuchILoveYou(nbPetals) {
+    let howMuchILoveYou=(nbPetals%6)
+            if (howMuchILoveYou===1)
+  {return ("I love you")}
+       
+    else if (howMuchILoveYou===2)
+  {return ("a little")}
+        
+    else if (howMuchILoveYou===3)
+  {return ("a lot")}
+          
+    else if (howMuchILoveYou===4)
+  {return ("passionately")}
+            
+    else if (howMuchILoveYou===5)
+  {return ("madly")}
+            
+    else if (howMuchILoveYou===0)
+  {return ("not at all")}
+    
+    }

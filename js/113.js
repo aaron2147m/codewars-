43cc81e370e0a70048a480cdb76bd6433f59fcd8
aaron2147m <aaronasuarez@yahoo.com>
2022-11-18 23:47:40 -0500

    114.js

--- js/113.js
+++ js/113.js
@@ -2,4 +2,4 @@ function roundIt(n) {
     var [a, b] = String(n).split('.').map(s => s.length);
     if (a < b) return Math.ceil(n);
     if (a > b) return Math.floor(n);
-    return Math.round(n);
+    return Math.round(n)};
---
+++ js/114.js
@@ -0,0 +1,4 @@
+function check(a,x){
+    let b = a.filter(el => el == x);
+    return b.length ? true : false;
+  };

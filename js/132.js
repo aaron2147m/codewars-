b27cb8d782c1e4bfd15e6bdade19a85071073df4
aaron2147m <aaronasuarez@yahoo.com>
2022-12-14 23:02:22 -0500

    132.js

---
+++ js/132.js
@@ -0,0 +1,8 @@
+let quote = function(fighter) {
+    switch(fighter.toLowerCase()) {
+      case 'conor mcgregor': return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+      case 'george saint pierre': return "I am not impressed by your performance.";
+      default : 'Who are you?' 
+    }
+  };
+  

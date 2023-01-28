72271d980146ef665573e980b7b38d41eceef30f
aaron2147m <aaronasuarez@yahoo.com>
2023-01-27 22:25:52 -0500

    145.js

---
+++ js/145.js
@@ -0,0 +1,9 @@
+function getCount(str) {
+    var vowelsCount = 0;
+    str.split("").forEach(function(x){
+      if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+        vowelsCount += 1;
+      }
+    });  
+    return vowelsCount;
+  }

3010faad1368dd1f7df802251d58ad9a444da69a
aaron2147m <aaronasuarez@yahoo.com>
2023-02-16 13:05:53 -0500

    149.js

---
+++ js/149.js
@@ -0,0 +1,6 @@
+//Check to see if a string has the same amount of 'x's and 'o's
+
+const XO = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+  }
